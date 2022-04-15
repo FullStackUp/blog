636e4c83dee77ff8c8986feaaef1a3ff5e8a7287
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-04-16 01:32:18 +0200

    Add mobile menu (topbar.js) and responsive

--- src/assets/js/topbar.js
+++ src/assets/js/topbar.js
@@ -1,1 +1,67 @@
-console.log("topbar");
+//We create the references for our menu and the menu icon :
+const iconMobile = document.querySelector(".header-menu-icon");
+const headerMenu = document.querySelector(".header-menu");
+//This property will let you know if the menu is open :
+let isMenuOpen = false;
+//This property will let you know if the mobile menu is created:
+let mobileMenuDOM;
+
+//To close the menu, simply remove the open class from the menu :
+const closeMenu = () => {
+  mobileMenuDOM.classList.remove("open");
+};
+
+//We create a div and we add the mobile-menu class.
+//We prevent the menu from closing on a click inside.
+//We then clone the normal menu links into it.
+const createMobileMenu = () => {
+  mobileMenuDOM = document.createElement("div");
+  mobileMenuDOM.classList.add("mobile-menu");
+  mobileMenuDOM.addEventListener("click", (event) => {
+    event.stopPropagation();
+  });
+  mobileMenuDOM.append(headerMenu.querySelector("ul").cloneNode(true));
+  headerMenu.append(mobileMenuDOM);
+};
+
+//If the menu is not created we create it.
+//In any case we open it by adding the open class :
+const openMenu = () => {
+  if (mobileMenuDOM) {
+  } else {
+    createMobileMenu();
+  }
+  mobileMenuDOM.classList.add("open");
+};
+
+//Allows you to open or close the menu depending on its status :
+const toggleMobileMenu = (event) => {
+  if (isMenuOpen) {
+    closeMenu();
+  } else {
+    openMenu();
+  }
+  isMenuOpen = !isMenuOpen;
+};
+
+//A click on the icon will open or close the menu
+//and prevent the event from being propagated to window :
+iconMobile.addEventListener("click", (event) => {
+  event.stopPropagation();
+  toggleMobileMenu();
+});
+
+//We recover clicks on window to close the menu.
+window.addEventListener("click", () => {
+  if (isMenuOpen) {
+    toggleMobileMenu();
+  }
+});
+
+//If the window is enlarged and it exceeds 480px in width
+//So we close the menu if it is open :
+window.addEventListener("resize", (event) => {
+  if (window.innerWidth > 480 && isMenuOpen) {
+    toggleMobileMenu();
+  }
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-//it's node.js :
+//It's node.js :
 
 const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
@@ -6,29 +6,29 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  //name of our entry and where is it
+  //Name of our entry and where is it :
   entry: {
     main: path.join(__dirname, "./src/index.js"),
     form: path.join(__dirname, "./src/form/form.js"),
     topbar: path.join(__dirname, "./src/assets/js/topbar.js"),
   },
-  //it's the out and where we're going put the transform from babel-loader = bundle
+  //It's the out and where we're going put the transform from babel-loader = bundle.
   output: {
     path: path.join(__dirname, "dist"),
     filename: "[name].bundle.js",
   },
-  //definy a key and rules
+  //Definy a key and rules :
   module: {
     rules: [
       {
-        //take all files js except node_modules
+        //Take all files js except node_modules.
         test: /\.js/,
         exclude: /(node_modules)/,
         use: ["babel-loader"],
       },
       {
         test: /\.scss$/i,
-        //starts from right to left, the order is important
+        //Starts from right to left, the order is important.
         use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
@@ -38,7 +38,7 @@ module.exports = {
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: path.join(__dirname, "./src/index.html"),
-      //own bundle
+      //Own bundle :
       chunks: ["main", "topbar"],
     }),
     new HtmlWebpackPlugin({
@@ -59,7 +59,7 @@ module.exports = {
   devtool: "source-map",
   mode: "development",
   devServer: {
-    //don't open if the navigator it's not open
+    //Don't open if the navigator it's not open.
     open: false,
     static: path.resolve(__dirname, "./dist"),
     port: 4000,
