a8f4db08c04b6e333ea604e96e9abc3a49c9b40d
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-04-15 11:52:55 +0200

    Create img and js folders, also update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,16 @@
 //it's node.js :
 
 const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   //name of our entry and where is it
   entry: {
     main: path.join(__dirname, "./src/index.js"),
     form: path.join(__dirname, "./src/form/form.js"),
+    topbar: path.join(__dirname, "./src/assets/js/topbar.js"),
   },
   //it's the out and where we're going put the transform from babel-loader = bundle
   output: {
@@ -31,16 +34,25 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
       filename: "index.html",
       template: path.join(__dirname, "./src/index.html"),
       //own bundle
-      chunks: ["main"],
+      chunks: ["main", "topbar"],
     }),
     new HtmlWebpackPlugin({
       filename: "form.html",
       template: path.join(__dirname, "./src/form/form.html"),
-      chunks: ["form"],
+      chunks: ["form", "topbar"],
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        {
+          from: "./src/assets/img/*",
+          to: "assets/img/[name][ext]",
+        },
+      ],
     }),
   ],
   stats: "minimal",
