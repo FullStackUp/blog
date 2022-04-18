a72ea44b13f242decd691f77410bd5c90d2edb42
FullStackUp <fullstackup.webdeveloper@gmail.com>
2022-04-18 16:52:35 +0200

    Add sort the articles by date (index.js)

--- src/index.js
+++ src/index.js
@@ -3,8 +3,15 @@ import "./index.scss";
 
 const articlesContainerElement = document.querySelector(".articles-container");
 const categoriesContainerElment = document.querySelector(".categories");
+const selectElement = document.querySelector("select");
 let filter;
 let articles;
+let sortBy = "desc";
+
+selectElement.addEventListener("change", () => {
+  sortBy = selectElement.value;
+  fetchArticles();
+});
 
 const createArticles = () => {
   const articlesDOM = articles
@@ -74,7 +81,10 @@ const createArticles = () => {
 const displayMenuCategories = (categoriesArr) => {
   const liElements = categoriesArr.map((categoryElem) => {
     const li = document.createElement("li");
-    li.innerHTML = `<li>${categoryElem[0]} (<strong>${categoryElem[1]}</strong>)</li>`;
+    li.innerHTML = `${categoryElem[0]} (<strong>${categoryElem[1]}</strong>)`;
+    if (categoryElem[0] === filter) {
+      li.classList.add("active");
+    }
     li.addEventListener("click", () => {
       if (filter === categoryElem[0]) {
         filter = null;
@@ -94,7 +104,6 @@ const displayMenuCategories = (categoriesArr) => {
 
   categoriesContainerElment.innerHTML = "";
   categoriesContainerElment.append(...liElements);
-  console.log(liElements);
 };
 
 const createMenuCategories = () => {
@@ -116,7 +125,9 @@ const createMenuCategories = () => {
 
 const fetchArticles = async () => {
   try {
-    const response = await fetch("https://restapi.fr/api/articles");
+    const response = await fetch(
+      `https://restapi.fr/api/articles?sort=createdAt:${sortBy}`
+    );
     articles = await response.json();
     createArticles();
     createMenuCategories();
